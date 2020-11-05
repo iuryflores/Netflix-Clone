2e900b0c92df0ebd7bc1658f4019cfc82061c6f2
Flores <iury.flores@1rigo.local>
2020-11-05 08:42:29 -0300

    first commit

---
+++ js/owl/main.js
@@ -0,0 +1,32 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+$('.owl-carousel-top').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:10
+        }
+    }
+})
